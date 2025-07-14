2f5e83b030004b8ab09d12cd5d3d53052bd3e9c0
Cristhian Odin Rueda Jiménez <cruedaji@montepiedad.com.mx>
2025-07-13 18:08:46 -0600

    Zoneless Angular

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
  templateUrl: './counter-page.component.html',
@@ -8,11 +8,27 @@ import { Component, signal } from "@angular/core";
     margin: 5px;
     width: 200px;
   }
- `
+ `,
+ // La siguiente línea es para desactivar ZoneJS (Hace más rápido el componente)
+ changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterPageComponent {
   counter = 10;
   counterSignal = signal(10)
+
+  constructor() {
+    setInterval(() => {
+      // Las propiedades del componente no se actualizan si ZoneJS está desactivado
+      // this.counter += 1;
+
+      // Las señales si se actualizan aún con ZoneJS desactivado
+      // this.counterSignal.update(v => v + 1);
+
+      // this.increaseBy(1);
+
+      console.log('Tick');
+    }, 2000);
+  }
 
   increaseBy(value: number) {
     this.counter += value;
