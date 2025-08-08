86213defb012759aa1420870c4f5fa7493c7ab7b
Cristhian Odin Rueda Jiménez <cruedaji@montepiedad.com.mx>
2025-08-08 16:25:53 -0600

    Despliegues y HashRouter

--- 02-bases/src/app/app.config.ts
+++ 02-bases/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
@@ -7,6 +8,15 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // HashStrategy
+    // Se ocupa para que en la ruta coloque un hash #
+    // de esta manera host en donde no tengamos el control total
+    // de las rutas, funciona el router
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 };
